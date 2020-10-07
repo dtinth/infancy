eecc881f3a3598575571674c83253863c8f20d73
Thai Pangsakulyanont <dtinth@spacet.me>
2020-10-07 15:08:24 +0000

    Add more controls

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,19 @@ export function typeVerifier<U>() {
 }
 
 export type Button = React.ComponentType<{
-  onClick: () => void
-  children: React.ReactNode
+  onClick?: () => void
+  children?: React.ReactNode
+  emphasis?: 'standard' | 'primary' | 'secondary' | 'positive' | 'negative'
+}>
+
+export type Panel = React.ComponentType<{
+  title?: React.ReactNode
+  bottomBar?: React.ReactNode
+  topBar?: React.ReactNode
+  children?: React.ReactNode
+}>
+
+export type Field = React.ComponentType<{
+  label?: React.ReactNode
+  children?: React.ReactNode
 }>
